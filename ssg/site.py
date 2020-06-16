39005d2de62a10cef2ec43ab3866904e8b92a0af
GeekTechDude <34283325+geektechdude@users.noreply.github.com>
2020-06-16 15:47:25 +0100

    Module 4

--- ssg/site.py
+++ ssg/site.py
@@ -1,14 +1,15 @@
 from pathlib import Path
+import sys
 
 class Site():
     """ The site class """
-    def __init__(self, source, dest, parsers=None):
+    def __init__(self, source: Path, dest: Path, parsers=None):
         """ Intialise with source and dest """
         self.source = Path(source)
         self.dest = Path(dest)
         self.parsers = parsers or []
     
-    def create_dir(self,path):
+    def create_dir(self,path: Path):
         """ Creates directory at path"""
         directory = self.dest / path.relative_to(self.source)
         directory.mkdir(parents=True, exist_ok=True)
@@ -37,5 +38,8 @@ class Site():
         if parser is not None:
             parser.parse(path,self.source, self.dest)
         else:
-            print("Not Implented")
+            self.error("No parser for the {} extension, file skipped!".format(path.suffix))
     
+    @staticmethod
+    def error(message):
+        sys.stderr.write("\x1b[1;31m{}\n".format(message))
